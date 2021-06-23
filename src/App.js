e41b9c72649ff7f01c3e570320ad5546d50f62c1
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 21:24:08 -0700

    imported and added Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Header from '';
+import Header from './components/Header/index';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 // import About from './pages/About';
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        {/* <Header /> */}
+        <Header />
         <Switch>
           <Route exact path='/Professional-Portfolio' component={Home} />
           {/* <Route exact path='/about' component={About} />
