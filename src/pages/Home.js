df739a8a0fb641812420f74d7b465bc0d53c2bf7
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 20:30:31 -0700

    imported bootstrap and added Grid components to the Home page and image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Container, Row, Col } from '../components/Grid';
-import Nav from '../components/Nav';
+import image from '../assets/images/profile-image.jpg';
 
 const Home = () => {
-
+    <Container fluid>
+        <Row>
+            <Col size='sm-12'>
+                <img src={image} alt='Cassandra Cunningham'/>
+            </Col>
+        </Row>
+    </Container>
 };
-
+  
 export default Home;
