43ea9eaf5ba3a0ccdeb5ff2a620ce919cfe17aa8
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 21:46:20 -0700

    added list item for about me icon

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import home from '../../assets/images/home.png';
+import about from '../../assets/images/about-me.png';
 import './style.css';
 
 const Nav = () => {
@@ -9,6 +10,12 @@ const Nav = () => {
                 <a className="icons" href="#header">
                     <img className="home" src={home} alt="House black icon" />
                     <p className="hidden">Home</p>
+                </a>
+            </li>
+            <li className='list-group-item'>
+                <a className="icons" href="#about-me">
+                    <img className="aboutme" src={about} alt="Faceless and armless upper torso with question mark on stomach black icon" />
+                    <p className="hidden">About Me</p>
                 </a>
             </li>
         </ul>
