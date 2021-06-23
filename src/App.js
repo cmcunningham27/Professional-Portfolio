1c57eb044fc9bab05526271109118f9f9eb8b38d
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 20:01:28 -0700

    imported all pages I will have in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contacts';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
   );
 }
 
