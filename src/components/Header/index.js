038d4dc69acd957d92e35f7ba210907a5b674178
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 21:20:47 -0700

    created Grid components and added h1 element for Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Container, Row, Col } from '../Grid';
 
 const Header = () => {
     return (
-        
+        <Container fluid>
+            <Row>
+                <Col size='sm-12'>
+                    <h1>Cassandra Cunningham</h1>
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
