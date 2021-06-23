a93403693fedc54ee75d0f9142108a5532528cbc
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 22:19:33 -0700

    changed each a element into Link from react-router-dom

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,10 @@ const Header = () => {
     return (
         <Container fluid>
             <Row>
-                <Col size='sm-7'>
-                    <h1>Cassandra Cunningham</h1>
+                <Col size='xs-12 sm-4 md-5 lg-7'>
+                    <h1 className='CMC'>Cassandra Cunningham</h1>
                 </Col>
-                <Col size='sm-5'>
+                <Col size='xs-12 sm-8 md-7 lg-5'>
                     <Nav />
                 </Col>
             </Row>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,45 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import home from '../../assets/images/home.png';
-import about from '../../assets/images/about-me.png';
-import project from '../../assets/images/my-work.png';
-import resume from '../../assets/images/resume.png';
-import contact from '../../assets/images/contact.png';
+// import about from '../../assets/images/about-me.png';
+// import project from '../../assets/images/my-work.png';
+// import resume from '../../assets/images/resume.png';
+// import contact from '../../assets/images/contact.png';
 import './style.css';
 
 const Nav = () => {
     return (
-        <ul className='list-group'>
-            <li className='list-group-item'>
-                <a className="icons" href="#header">
-                    <img className="home" src={home} alt="House black icon" />
-                    <p className="hidden">Home</p>
-                </a>
-            </li>
-            <li className='list-group-item'>
-                <a className="icons" href="#about-me">
-                    <img className="aboutme" src={about} alt="Faceless and armless upper torso with question mark on stomach black icon" />
-                    <p className="hidden">About Me</p>
-                </a>
-            </li>
-            <li className='list-group-item'>
-                <a className="icons" href="#projects">
-                    <img className="mywork" src={project} alt="Lightbulb black icon" />
-                    <p className="hidden">Projects</p>
-                </a>
-            </li>
-            <li>
-                <a class="icons" href="./assets/resume/CMC_Resume.pdf" target="_blank">
-                    <img class="resume" src={resume} alt="Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon" />
-                    <p class="hidden">Resume</p>
-                </a>
-            </li>
-            <li className='list-group-item'>
-                <a className="icons"href="#contact">
-                    <img className="contact" src={contact} alt="Contact book with faceless and armless upper torso black icon" />
-                    <p className="hidden">Contact</p>
-                </a>
-            </li>
-        </ul>
+        <div>
+            <Link className="icons" to="/Professional-Portfolio">
+                <img className="home" src={home} alt="House black icon" />
+                {/* <p className="hidden">Home</p> */}
+            </Link>
+            {/* <a className="icons" href="#about-me">
+                <img className="aboutme" src={about} alt="Faceless and armless upper torso with question mark on stomach black icon" />
+                {/* <p className="hidden">About Me</p> */}
+            {/* </a>
+            <a className="icons" href="#projects">
+                <img className="mywork" src={project} alt="Lightbulb black icon" />
+                {/* <p className="hidden">Projects</p> */}
+            {/* </a> */}
+            {/* <a className="icons" href="./assets/resume/CMC_Resume.pdf" target="_blank">
+                <img className="resume" src={resume} alt="Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon" />
+                {/* <p className="hidden">Resume</p> */}
+            {/* </a> */} */}
+            {/* <a className="icons"href="#contact">
+                <img className="contact" src={contact} alt="Contact book with faceless and armless upper torso black icon" />
+                {/* <p className="hidden">Contact</p> */}
+            {/* </a> */} */} */} */}
+        </div>
     )
 };
 
