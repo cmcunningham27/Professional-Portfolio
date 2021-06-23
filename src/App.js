a3792315b3aec481cd15578a2a1083c235255bf9
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 20:08:11 -0700

    imported and added Header and Footer components to be used later

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-// import logo from './logo.svg';
 import React from 'react';
+// import Header from '';
 import { Router, Route, Switch } from 'react-router';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contacts';
+// import Footer from '';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        
+        {/* <Header /> */}
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
           <Route exact path='/projects' component={Projects} />
           <Route exact path='/contact' component={Contact} />
         </Switch>
+        {/* <Footer /> */}
       </div>
     </Router>
   );
