518057b3cab3554c5bd17b8a8badb1c6da5d3806
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 21:15:30 -0700

    fixed rendering problem for Home page by including return()

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 // import Header from '';
-import { Router, Route, Switch } from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
 // import Footer from '';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className='App'>
         {/* <Header /> */}
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
+          <Route exact path='/Professional-Portfolio' component={Home} />
+          {/* <Route exact path='/about' component={About} />
           <Route exact path='/projects' component={Projects} />
-          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/contact' component={Contact} /> */}
         </Switch>
         {/* <Footer /> */}
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,15 @@ import { Container, Row, Col } from '../components/Grid';
 import image from '../assets/images/profile-image.jpg';
 
 const Home = () => {
-    <Container fluid>
-        <Row>
-            <Col size='sm-12'>
-                <img src={image} alt='Cassandra Cunningham'/>
-            </Col>
-        </Row>
-    </Container>
+    return (
+        <Container fluid>
+            <Row>
+                <Col size='sm-12'>
+                    <img className='profileImage rounded-circle' src={image} alt='Cassandra Cunningham'/>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
   
 export default Home;
