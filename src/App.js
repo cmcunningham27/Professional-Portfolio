618afafe72c5db1499e8d5e2715c7253a5091226
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-22 20:06:04 -0700

    imported Router, Route and Switch to create Routes for each page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react';
+import { Router, Route, Switch } from 'react-router';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -8,7 +9,17 @@ import './App.css';
 
 function App() {
   return (
-    
+    <Router>
+      <div className='App'>
+        
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
