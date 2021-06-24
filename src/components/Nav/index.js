bef075011c1dc824fd195bfd55ce55890c8512e7
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-23 18:03:14 -0700

    turned all other a elements into Link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,22 +14,22 @@ const Nav = () => {
                 <img className="home" src={home} alt="House black icon" />
                 {/* <p className="hidden">Home</p> */}
             </Link>
-            {/* <a className="icons" href="#about-me">
+            <Link className="icons" href="/About">
                 <img className="aboutme" src={about} alt="Faceless and armless upper torso with question mark on stomach black icon" />
                 {/* <p className="hidden">About Me</p> */}
-            {/* </a>
-            <a className="icons" href="#projects">
+            </Link>
+            <Link className="icons" href="/Projects">
                 <img className="mywork" src={project} alt="Lightbulb black icon" />
-                {/* <p className="hidden">Projects</p> */}
-            {/* </a> */}
-            {/* <a className="icons" href="./assets/resume/CMC_Resume.pdf" target="_blank">
+                {/* <p className="hidden">Projects</p>*/}
+            </Link> 
+            <Link className="icons" href="./assets/resume/CMC_Resume.pdf" target="_blank">
                 <img className="resume" src={resume} alt="Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon" />
                 {/* <p className="hidden">Resume</p> */}
-            {/* </a> */} */}
-            {/* <a className="icons"href="#contact">
+            </Link>
+            <Link className="icons"href="/Contact">
                 <img className="contact" src={contact} alt="Contact book with faceless and armless upper torso black icon" />
                 {/* <p className="hidden">Contact</p> */}
-            {/* </a> */} */} */} */}
+            </Link>
         </div>
     )
 };
