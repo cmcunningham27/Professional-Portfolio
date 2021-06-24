646166a82b3257f86df013ffda9660fd70d4a738
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-23 21:52:32 -0700

    created Projects functional component with simple h1 element and exported it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 // import About from './pages/About';
 // import Projects from './pages/Projects';
-// import Contact from './pages/Contact';
+import Contact from './pages/Contact';
 // import Footer from '';
 import './App.css';
 
@@ -15,9 +15,9 @@ function App() {
         <Header />
         <Switch>
           <Route exact path='/Professional-Portfolio' component={Home} />
-          {/* <Route exact path='/about' component={About} />
-          <Route exact path='/projects' component={Projects} />
-          <Route exact path='/contact' component={Contact} /> */}
+          {/* <Route exact path='/about' component={About} /> */}
+          {/* <Route exact path='/projects' component={Projects} /> */}
+          <Route exact path='/contact' component={Contact} />
         </Switch>
         {/* <Footer /> */}
       </div>
