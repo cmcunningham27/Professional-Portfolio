0c11685b7a26181f8eaa894f6e8e8d5122fa8df5
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-23 22:12:33 -0700

    added Resume pdf file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header/index';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-// import About from './pages/About';
+import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 // import Footer from '';
@@ -15,9 +15,9 @@ function App() {
         <Header />
         <Switch>
           <Route exact path='/Professional-Portfolio' component={Home} />
-          {/* <Route exact path='/about' component={About} /> */}
+          <Route exact path='/about' component={About} />
           <Route exact path='/projects' component={Projects} />
-          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/Professional-Portfolio/contact' component={Contact} />
         </Switch>
         {/* <Footer /> */}
       </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,15 +10,15 @@ import './style.css';
 const Nav = () => {
     return (
         <div>
-            <Link className="icons" to="/Professional-Portfolio">
+            <Link className='icons {location.pathname === "/" ? "nav.link active" : "nav-link"}' to='/Professional-Portfolio'>
                 <img className="home" src={home} alt="House black icon" />
                 {/* <p className="hidden">Home</p> */}
             </Link>
-            <Link className="icons" href="/About">
+            <Link className="icons" href="/about">
                 <img className="aboutme" src={about} alt="Faceless and armless upper torso with question mark on stomach black icon" />
                 {/* <p className="hidden">About Me</p> */}
             </Link>
-            <Link className="icons" href="/Projects">
+            <Link className="icons" href="/projects">
                 <img className="mywork" src={project} alt="Lightbulb black icon" />
                 {/* <p className="hidden">Projects</p>*/}
             </Link> 
@@ -26,7 +26,7 @@ const Nav = () => {
                 <img className="resume" src={resume} alt="Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon" />
                 {/* <p className="hidden">Resume</p> */}
             </Link>
-            <Link className="icons"href="/Contact">
+            <Link className="icons"href="/Professional-Portfolio/contact">
                 <img className="contact" src={contact} alt="Contact book with faceless and armless upper torso black icon" />
                 {/* <p className="hidden">Contact</p> */}
             </Link>
