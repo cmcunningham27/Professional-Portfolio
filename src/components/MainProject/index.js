888b6d00d3143ce92729c520b3dbcbd7e05fd92a
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 11:37:33 -0700

    updated link pathway to resume

--- src/components/MainProject/index.js
+++ src/components/MainProject/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Moimeries from '../../assets/images/MOI-ME-RIES_photo.png'
 
 const MainProject = () => {
     return (
         <div className='MainProject'>
             <h3 class='title'>MOI-ME-RIES App</h3>
             <a href='https://blooming-ocean-65385.herokuapp.com/' target='_blank' rel='noreferrer'>
-                <img class='project1-img' src='./assets/images/MOI-ME-RIES_photo.png' alt='Login page for MOI-ME-RIES app.' />
+                <img class='project1-img' src={Moimeries} alt='Login page for MOI-ME-RIES app.' />
             </a>
             <button class='github-btn'>
                 <a href='https://github.com/cmcunningham27/moi-me-ries' target='_blank' rel='noreferrer'>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,29 +5,30 @@ import about from '../../assets/images/about-me.png';
 import project from '../../assets/images/my-work.png';
 import resume from '../../assets/images/resume.png';
 import contact from '../../assets/images/contact.png';
+import Resume from '../../assets/resume';
 import './style.css';
 
 const Nav = () => {
     return (
         <div>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio' exact>
-                <img className="home" src={home} alt="House black icon" />
+                <img className='home' src={home} alt='House black icon' />
                 {/* <p className="hidden">Home</p> */}
             </NavLink>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio/about' exact>
-                <img className="aboutme" src={about} alt="Faceless and armless upper torso with question mark on stomach black icon" />
+                <img className='aboutme' src={about} alt='Faceless and armless upper torso with question mark on stomach black icon' />
                 {/* <p className="hidden">About Me</p> */}
             </NavLink>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio/projects' exact>
-                <img className="mywork" src={project} alt="Lightbulb black icon" />
+                <img className='mywork' src={project} alt='Lightbulb black icon' />
                 {/* <p className="hidden">Projects</p>*/}
             </NavLink> 
-            <Link className="icons" href="./assets/resume/CMC_Resume.pdf" target="_blank">
-                <img className="resume" src={resume} alt="Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon" />
+            <Link className='icons' href={Resume} target='_blank' rel='noreferrer'>
+                <img className='resume' src={resume} alt='Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon' />
                 {/* <p className="hidden">Resume</p> */}
             </Link>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio/contact' exact>
-                <img className="contact" src={contact} alt="Contact book with faceless and armless upper torso black icon" />
+                <img className='contact' src={contact} alt='Contact book with faceless and armless upper torso black icon' />
                 {/* <p className="hidden">Contact</p> */}
             </NavLink>
         </div>
