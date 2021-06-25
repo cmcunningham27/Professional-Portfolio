d6428ee8899eac3a49254f9654ab78801e676cc5
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 11:32:11 -0700

    added button that links to Github repository

--- src/components/MainProject/index.js
+++ src/components/MainProject/index.js
@@ -7,6 +7,11 @@ const MainProject = () => {
             <a href='https://blooming-ocean-65385.herokuapp.com/' target='_blank' rel='noreferrer'>
                 <img class='project1-img' src='./assets/images/MOI-ME-RIES_photo.png' alt='Login page for MOI-ME-RIES app.' />
             </a>
+            <button class='github-btn'>
+                <a href='https://github.com/cmcunningham27/moi-me-ries' target='_blank' rel='noreferrer'>
+                    GitHub repository
+                </a>
+            </button>
         </div>
     )
 };
