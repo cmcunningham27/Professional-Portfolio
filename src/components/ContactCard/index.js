cb6f27b6149c03d287761d3f6ccc219389021a1f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 11:18:11 -0700

    added unordered list with 4 list items, first one with link to email

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -2,7 +2,24 @@ import React from ' react';
 
 const ContactCard = () => {
     return (
+        <div className="ContactCard">
+            <ul className='list-group'>
+                <li className='list-group-item'>
+                    <h5>
+                        <a href='mailto:sttepstutoring@yahoo.com'>Send E-mail</a>
+                    </h5>
+                </li>
+                <li className='list-group-item'>
 
+                </li>
+                <li className='list-group-item'>
+
+                </li>
+                <li className='list-group-item'>
+
+                </li>
+            </ul>
+        </div>
     )
 };
 
