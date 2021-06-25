1789bfd9ba828309a2c4e039bcc58ec91e024a5c
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 12:47:34 -0700

    switched componentDidMount to useEffect and utilizing useState for API call

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,39 +1,44 @@
-import React, { Components } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from '../components/Grid';
 import MainProject from '../components/MainProject';
 import API from '../utils/API';
 
-class Projects extends Components {
-    componentDidMount() {
-        API.getInfo()
-            .then((res) => console.log(res))
-            .catch((err) => console.log(err));
-    };
+const Projects = () => {
+    const [repos, setRepos] = useState([]);
 
-    render() {
-       return (
-            <Container fluid>
-                <Row>
-                    <Col size='sm-12'>
-                        <h1>
-                            My Projects
-                        </h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size='sm-12'>
-                        <MainProject />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size='sm-12'>
+    useEffect(() => {
+        API.getInfo()
+            .then((res) => setRepos({ 
+                starredRepos: res.data.map((e, i) => ({
+                    key: i,
+                    name: e.name,
+                    link: e.svn_url,
+                }))
+            }))
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col size='sm-12'>
+                    <h1>
+                        My Projects
+                    </h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col size='sm-12'>
+                    <MainProject />
+                </Col>
+            </Row>
+            <Row>
+                <Col size='sm-12'>
                         
-                    </Col>
-                </Row>
-            </Container>
-        ) 
-    }
-    
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 export default Projects;
