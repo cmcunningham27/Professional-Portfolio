dec208e7dcd0d4c469b9b77958f82035aa3ab1de
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 13:03:19 -0700

    created SmallProject folder and index.js, declared SmallProject functional component and exported it with an h5 element

---
+++ src/components/SmallProject/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SmallProject = () => {
+    return (
+        <div className='smallProject'>
+            <h5>
+                
+            </h5>
+        </div>
+    )
+};
+
+export default SmallProject;
