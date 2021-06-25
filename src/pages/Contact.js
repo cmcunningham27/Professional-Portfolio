85c77bc2c5f868ed76a1be44640c717e0ad5586c
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 11:11:17 -0700

    created ContactCard folder and index.js file, declared ContactCard functional component and exported it

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Container, Row, Col } from '../components/Grid';
 
+
 const Contact = () => {
     return (
         <Container fluid>
             <Row>
                 <Col size='sm-12'>
-                    
+                    <h1>
+                        Contacts
+                    </h1>
                 </Col>
             </Row>
             <Row>
                 <Col size='sm-12'>
-                    
+                  
                 </Col>
             </Row>
         </Container>
