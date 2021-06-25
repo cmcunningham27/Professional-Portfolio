70c2bb7a7b5e0bd6dd3e3284cd15ff97eea0ef90
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 12:18:30 -0700

    turned Projects from stateless functional component to class component; imported API and in componentDidMount make an API call

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { Components } from 'react';
 import { Container, Row, Col } from '../components/Grid';
 import MainProject from '../components/MainProject';
+import API from '../utils/API';
 
-const Projects = () => {
+class Projects extends Components {
+    componentDidMount() {
+        API.getInfo()
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    };
+
+    render() {
+       return (
+            <Container fluid>
+                <Row>
+                    <Col size='sm-12'>
+                        <h1>
+                            My Projects
+                        </h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size='sm-12'>
+                        <MainProject />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size='sm-12'>
+                        
+                    </Col>
+                </Row>
+            </Container>
+        ) 
+    }
     
-    return (
-        <Container fluid>
-            <Row>
-                <Col size='sm-12'>
-                    <h1>
-                        My Projects
-                    </h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col size='sm-12'>
-                    <MainProject />
-                </Col>
-            </Row>
-            <Row>
-                <Col size='sm-12'>
-                    
-                </Col>
-            </Row>
-        </Container>
-    )
 };
 
 export default Projects;
