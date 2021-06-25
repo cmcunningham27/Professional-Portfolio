142ca97a7fce811e339bc3240f0e6f7e88888271
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 13:09:27 -0700

    added anchor and button links as well as classnames

--- src/components/SmallProject/index.js
+++ src/components/SmallProject/index.js
@@ -3,9 +3,17 @@ import React from 'react';
 const SmallProject = () => {
     return (
         <div className='smallProject'>
-            <h5>
-                
+            <h5 class="title">
+
             </h5>
+            <a href='' target='_blank' rel='noreferrer'>
+                <img class="project-img"/>
+            </a>
+            <button class="github-btn">
+                <a href='' target='_blank' rel='noreferrer'>
+                    GitHub repository
+                </a>
+            </button>
         </div>
     )
 };
