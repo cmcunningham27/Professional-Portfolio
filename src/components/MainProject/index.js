7611d3d3b5b696539bddc59d0fd4b9801342e753
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-25 11:30:57 -0700

    added h3 element and anchor tag with img

--- src/components/MainProject/index.js
+++ src/components/MainProject/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const MainProject = () => {
     return (
-
+        <div className='MainProject'>
+            <h3 class='title'>MOI-ME-RIES App</h3>
+            <a href='https://blooming-ocean-65385.herokuapp.com/' target='_blank' rel='noreferrer'>
+                <img class='project1-img' src='./assets/images/MOI-ME-RIES_photo.png' alt='Login page for MOI-ME-RIES app.' />
+            </a>
+        </div>
     )
 };
 
