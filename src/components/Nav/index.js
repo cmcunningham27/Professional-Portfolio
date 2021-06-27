8bf4bda63a7138637d89075ed74cb53ad7c68554
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-27 13:20:37 -0700

    cleaned up Nav index.js

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,23 +13,18 @@ const Nav = () => {
         <div>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio' exact>
                 <img className='home' src={home} alt='House black icon' />
-                {/* <p className="hidden">Home</p> */}
             </NavLink>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio/about' exact>
                 <img className='aboutme' src={about} alt='Faceless and armless upper torso with question mark on stomach black icon' />
-                {/* <p className="hidden">About Me</p> */}
             </NavLink>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio/projects' exact>
                 <img className='mywork' src={project} alt='Lightbulb black icon' />
-                {/* <p className="hidden">Projects</p>*/}
             </NavLink> 
             <NavLink className='icons' to={Resume} target='_blank' rel='noreferrer'>
                 <img className='resume' src={resume} alt='Sheet of paper with women torso and lines below, and a magnifiying glass containing a check mark black icon' />
-                {/* <p className="hidden">Resume</p> */}
             </NavLink>
             <NavLink className='icons navbar-item' activeClassName='is-active' to='/Professional-Portfolio/contact' exact>
                 <img className='contact' src={contact} alt='Contact book with faceless and armless upper torso black icon' />
-                {/* <p className="hidden">Contact</p> */}
             </NavLink>
         </div>
     )
