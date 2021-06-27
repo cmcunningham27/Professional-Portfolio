81f6a235f8a7910d8205e66a4165b3d4208e6bbc
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-27 12:51:07 -0700

    commented API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//collects all of my starred repos
 const API  = {
     getInfo: function() {
         return axios.get('https://api.github.com/users/cmcunningham27/starred')
