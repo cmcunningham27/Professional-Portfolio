bc1266ae350c0245bd5bf1bd4a03884be02e485b
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-08-22 14:26:37 -0700

    added summaries for each switch case and inserted it in component

--- src/components/SmallProject/index.js
+++ src/components/SmallProject/index.js
@@ -11,6 +11,7 @@ import './style.css';
 let photo;
 let git;
 let info;
+let summary;
 
 const SmallProject = ({ name, link }) => {
     // sets attributes based on which project was collected at the time
@@ -19,26 +20,31 @@ const SmallProject = ({ name, link }) => {
             photo = Online;
             git = 'https://safe-dawn-19198.herokuapp.com';
             info = 'Indexed DB and list of transactions';
+            summary = 'An App that allows users to keep track of their budget when online and offline.';
             break;
         case 'Employee-Directory_React':
             photo = Directory;
             git = 'https://cmcunningham27.github.io/Employee-Directory_React';
             info = 'Employees beginning with letter b';
+            summary = 'A React app that allows its users to view all employees in a directory and search by typing in a name or sort employees by Name or Phone.';
             break;
         case 'MySQL-Employee_Tracker':
             photo = Tracker;
             git = link;
             info = '3 tables: all employees, employees by department, and employees by manager';
+            summary = 'CMS that allows Managers the ability to organize and plan their business. Managers can view and manage the departments, roles, and employees in their company';
             break;
         case 'MVC-Tech_Blog':
             photo = Blog;
             git = 'https://safe-river-08906.herokuapp.com';
             info = 'blog post for Mailbox Peak';
+            summary = 'An app to publish blog posts, my thoughts and feelings.';
             break;
         case 'Whats-the-Song-app':
             photo = Song;
             git = 'https://cmcunningham27.github.io/Whats-the-Song-app';
             info = 'Spotify Login to begin searching for songs';
+            summary = 'Music application that gives you the ability to search for music by: Lyrics, Artist, Song Title, or Album.';
             break;
         case 'Google-Books-Search':
             photo = Books;
@@ -49,13 +55,14 @@ const SmallProject = ({ name, link }) => {
             photo = Moimeries;
             git = 'https://blooming-ocean-65385.herokuapp.com/';
             info = 'Login page';
+            summary = 'An app to share bucket-list events both past and future.';
             break;
         default:
             break;
     };
 
     return (
-        <div className='smallProject d-flex flex-column'>
+        <div className='smallProject d-flex flex-column align-items-center'>
             <a href={git} target='_blank' rel='noreferrer'>
                 <img className="project-img" src={photo} alt={info}/>
             </a>
@@ -68,6 +75,9 @@ const SmallProject = ({ name, link }) => {
                         GitHub repository
                     </a>
                 </button>
+                <p>
+                    {summary}
+                </p>
             </div> 
         </div>
     )
